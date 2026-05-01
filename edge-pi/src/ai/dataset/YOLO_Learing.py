1995e9bff1ce255f0d372b7138d89b219336d21a
허진혁 <point0986@naver.com>
2026-05-01 15:43:49 +0900

    Yolo Learing start
    YOLO 모델을 로드해서 모델 학습 시작

---
+++ edge-pi/src/ai/dataset/YOLO_Learing.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+def main():
+    # 세그멘테이션용 YOLO 모델 로드
+    model = YOLO('yolo11n-seg.pt')
+
+    # 모델 학습 시작
+    results = model.train(
+        data='data.yaml',            # yaml 파일 경로
+        epochs=100,                  # 학습 반복 횟수
+        imgsz=640,                   # 이미지 크기
+        batch=16,                    # 배치 사이즈
+        device='0',                  # GPU 사용
+        project='kickboard_safety',  # 프로젝트(저장) 폴더명
+        name='street_sidewalk_seg'   # 실험 이름
+    )
+
+    print("모델 학습 완료")
+
+if __name__ == '__main__':
+    main()
